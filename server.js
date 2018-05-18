7aa94fc24c41d988d46281294648f47e40c2ccd6
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-18 10:50:20 -0400

    Implemented GraphQLSchema constructor in server code

--- server.js
+++ server.js
@@ -1,135 +1,203 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+// var { buildSchema } = require('graphql');
+var graphql = require('graphql');
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
-  }
+// // Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type RandomDie {
+//     numSides: Int!
+//     rollOnce: Int!
+//     roll(numRolls: Int!): [Int]
+//   }
 
-  input MessageInput {
-    content: String
-    author: String
-  }
+//   input MessageInput {
+//     content: String
+//     author: String
+//   }
 
-  type Message {
-    id: ID!
-    content: String
-    author: String
-  }
+//   type Message {
+//     id: ID!
+//     content: String
+//     author: String
+//   }
 
-  type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID!, input: MessageInput): Message
-  }
+//   type User {
+//     id: String
+//     name: String
+//   }
 
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-    rollDice(numDice: Int!, numSides: Int): [Int]
-    getDie(numSides: Int): RandomDie
-    getMessage(id: ID!): Message
-    ip: String
-  }
-`);
+//   type Mutation {
+//     createMessage(input: MessageInput): Message
+//     updateMessage(id: ID!, input: MessageInput): Message
+//   }
 
-// This class implements the RandomDie GraphQL type
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
+//   type Query {
+//     quoteOfTheDay: String
+//     random: Float!
+//     rollThreeDice: [Int]
+//     rollDice(numDice: Int!, numSides: Int): [Int]
+//     getDie(numSides: Int): RandomDie
+//     getMessage(id: ID!): Message
+//     ip: String
+//     user(id: String): User
+//   }
+// `);
 
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
+// // This class implements the RandomDie GraphQL type
+// class RandomDie {
+//   constructor(numSides) {
+//     this.numSides = numSides;
+//   }
 
-  roll({numRolls}) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
-  }
-}
+//   rollOnce() {
+//     return 1 + Math.floor(Math.random() * this.numSides);
+//   }
 
-// If Message had any complex fields, we'd put them on this object.
-class Message {
-  constructor(id, {content, author}) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
-  }
-}
+//   roll({numRolls}) {
+//     var output = [];
+//     for (var i = 0; i < numRolls; i++) {
+//       output.push(this.rollOnce());
+//     }
+//     return output;
+//   }
+// }
 
-// Create the database
-var fakeDatabase = {};
+// // If Message had any complex fields, we'd put them on this object.
+// class Message {
+//   constructor(id, {content, author}) {
+//     this.id = id;
+//     this.content = content;
+//     this.author = author;
+//   }
+// }
 
-// Express middleware
-function loggingMiddleware(req, res, next) {
-  console.log('ip:', req.ip);
-  next();
-}
+// // Create databases
+// var fakeMessageDatabase = {};
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+// var fakeUserDatabase = {
+//   'a': {
+//     id: 'a',
+//     name: 'alice',
+//   },
+//   'b': {
+//     id: 'b',
+//     name: 'bob',
+//   },
+// };
+
+// // Express middleware
+// function loggingMiddleware(req, res, next) {
+//   console.log('ip:', req.ip);
+//   next();
+// }
+
+// // The root provides a resolver function for each API endpoint
+// var root = {
+//   quoteOfTheDay: () => {
+//     return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+//   },
+//   random: () => {
+//     return Math.random();
+//   },
+//   rollThreeDice: () => {
+//     return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+//   },
+//   rollDice: ({numDice, numSides}) => {
+//     var output = [];
+//     for (var i = 0; i < numDice; i++) {
+//       output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+//     }
+//     return output;
+//   },
+//   getDie: ({numSides}) => {
+//     return new RandomDie(numSides || 6);
+//   },
+//   getMessage: function ({id}) {
+//     if (!fakeDatabase[id]) {
+//       throw new Error('no message exists with id ' + id);
+//     }
+//     return new Message(id, fakeDatabase[id]);
+//   },
+//   createMessage: function ({input}) {
+//     // Create a random id for our "database".
+//     var id = require('crypto').randomBytes(10).toString('hex');
+
+//     fakeMessageDatabase[id] = input;
+//     return new Message(id, input);
+//   },
+//   updateMessage: function ({id, input}) {
+//     if (!fakeMessageDatabase[id]) {
+//       throw new Error('no message exists with id ' + id);
+//     }
+//     // This replaces all old data, but some apps might want partial update.
+//     fakeMessageDatabase[id] = input;
+//     return new Message(id, input);
+//   },
+//   ip: (args, request) => {
+//     return request.ip;
+//   },
+//   user: ({id}) => {
+//     return fakeUserDatabase[id];
+//   }
+// };
+
+// var app = express();
+// app.use(loggingMiddleware);
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true,
+// }));
+// app.listen(4000);
+// console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+// Maps id to User object
+var fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice',
   },
-  random: () => {
-    return Math.random();
+  'b': {
+    id: 'b',
+    name: 'bob',
   },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-  },
-  rollDice: ({numDice, numSides}) => {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
-  },
-  getDie: ({numSides}) => {
-    return new RandomDie(numSides || 6);
-  },
-  getMessage: function ({id}) {
-    if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id);
-    }
-    return new Message(id, fakeDatabase[id]);
-  },
-  createMessage: function ({input}) {
-    // Create a random id for our "database".
-    var id = require('crypto').randomBytes(10).toString('hex');
-
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
-  updateMessage: function ({id, input}) {
-    if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id);
-    }
-    // This replaces all old data, but some apps might want partial update.
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
-  ip: (args, request) => {
-    return request.ip;
-  }
 };
 
+// Define the User type
+var userType = new graphql.GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString },
+  }
+});
+
+// Define the Query type
+var queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      // `args` describes the arguments that the `user` query accepts
+      args: {
+        id: { type: graphql.GraphQLString }
+      },
+      resolve: function (_, {id}) {
+        return fakeDatabase[id];
+      }
+    }
+  }
+});
+
+var schema = new graphql.GraphQLSchema({query: queryType});
+
 var app = express();
-app.use(loggingMiddleware);
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 app.listen(4000);
-
 console.log('Running a GraphQL API server at localhost:4000/graphql');
 
 // Use the following to test the application:
@@ -143,7 +211,10 @@ console.log('Running a GraphQL API server at localhost:4000/graphql');
 //     rollOnce
 //     roll(numRolls: 3)
 //   },
-//   ip
+//   ip,
+//   user(id: "a") {
+//     name
+//   }
 // }
 
 // mutation {
