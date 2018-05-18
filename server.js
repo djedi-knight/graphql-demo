2308e43225547d28221ee49b8e45683d9b6369d4
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-05-17 22:46:26 -0400

    Implemented mutations and input types in the server code

--- server.js
+++ server.js
@@ -10,12 +10,29 @@ var schema = buildSchema(`
     roll(numRolls: Int!): [Int]
   }
 
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+
   type Query {
     quoteOfTheDay: String
     random: Float!
     rollThreeDice: [Int]
     rollDice(numDice: Int!, numSides: Int): [Int]
-    getDie(numSides: Int): RandomDie
+    getDie(numSides: Int): RandomDie,
+    getMessage(id: ID!): Message
   }
 `);
 
@@ -38,6 +55,18 @@ class RandomDie {
   }
 }
 
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+// Create the database
+var fakeDatabase = {};
+
 // The root provides a resolver function for each API endpoint
 var root = {
   quoteOfTheDay: () => {
@@ -58,6 +87,27 @@ var root = {
   },
   getDie: ({numSides}) => {
     return new RandomDie(numSides || 6);
+  },
+  getMessage: function ({id}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function ({id, input}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
   }
 };
 
